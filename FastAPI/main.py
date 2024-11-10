b31cd6ab817ba0e2bf2ddff6b8e510e9bf1e2552
Erdem GUNSELI <85558041+ErdemGunseli@users.noreply.github.com>
2024-11-10 14:34:00 +0000

    Update main.py

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -1,3 +1,5 @@
+import re
+
 from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 from slowapi.middleware import SlowAPIMiddleware
@@ -7,7 +9,6 @@ from slowapi.errors import RateLimitExceeded
 from slowapi import _rate_limit_exceeded_handler
 from dotenv import load_dotenv
 import uvicorn
-
 
 # Loading environment variables and declaring FastAPI instance before local imports:
 load_dotenv()
@@ -39,7 +40,6 @@ app.add_middleware(SlowAPIMiddleware)
 # Creating tables if they don't already exist:
 models.Base.metadata.create_all(bind=engine)
 
-
 # Including routers:
 app.include_router(assistant.router)
 app.include_router(auth.router)
@@ -56,39 +56,24 @@ async def validation_exception_handler(request: Request, exc: RequestValidationE
         field = " -> ".join([
             str(elem).replace('_', ' ').capitalize() for elem in error['loc'] if elem != 'body'
         ])
-        # Customize the error message to remove type information
-        error_code = error['type']
-        message = ""
-        if error_code == "value_error.missing":
-            message = "This field is required."
-        elif error_code.startswith("type_error"):
-            message = "Invalid value."
-        elif error_code == "value_error.any_str.min_length":
-            min_length = error['ctx']['limit_value']
-            message = f"Ensure this value has at least {min_length} characters."
-        elif error_code == "value_error.any_str.max_length":
-            max_length = error['ctx']['limit_value']
-            message = f"Ensure this value has at most {max_length} characters."
-        else:
-            message = error['msg'].capitalize()
+        # Get the error message and capitalize it
+        message = error['msg'].capitalize()
+        # Remove the last word (the type) and preceding space and colon
+        message = re.sub(r'[:]\s+\w+$', '', message)
         errors.append(f"{field}: {message}")
-    friendly_message = " ".join(errors)
     return JSONResponse(
         status_code=422,
-        content={"detail": friendly_message}
+        content={"detail": " ".join(errors)}
     )
 
 @app.get("/", response_class=PlainTextResponse, include_in_schema=False)
 async def root():
     return "Welcome to the In-Sight API! You can send requests to this URL. For documentation, please visit https://api.in-sight.ai/docs."
 
-
 # TODO: Incorporate Video
 # TODO: Incorporate NLP & ML
 # TODO: Incorporate RealTime
 # TODO: After RealTime, Endpoint to change voice type, detail length, voice speed
-
-
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
